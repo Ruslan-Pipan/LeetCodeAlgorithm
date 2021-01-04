f7435dcbf4c932b40743006552505b523fd733bc
ruspi <rus.pipan@ukr.net>
2021-01-04 21:48:37 +0200

    Answer on algorithm "Two Sum".

--- src/main/java/com/leetCode/algorithms/TwoSum.java
+++ src/main/java/com/leetCode/algorithms/TwoSum.java
@@ -1,7 +1,9 @@
 package com.leetCode.algorithms;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 /**
  *
@@ -26,7 +28,10 @@ import java.util.Map;
 
 public class TwoSum {
 
-    public static int[] twoSumResolvedByLoops(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target){
+        if (nums.length == 2 && nums[0] + nums[1] == target)
+            return new int[] {0,1};
+
         for (int i = 0; i < nums.length - 1; i++){
             for (int j = i + 1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target){
@@ -37,24 +42,12 @@ public class TwoSum {
         throw new IllegalArgumentException("No found.");
     }
 
-    public static int[] twoSumResolvedByMap(int[] nums, int target){
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int number = target-nums[i];
-            System.out.println(target + " - " + nums[i] + " = " + number);
-            if (map.containsValue(number) && map.get(number) != i)
-                return new int[] {map.get(number),i};
-        }
-        throw new IllegalArgumentException("No found.");
-    }
+
 
     public static void main(String[] args) {
-        int[]nums = {3,2,4};
+        int[]nums = {3,3};
         int target = 6;
-        int[] result = twoSumResolvedByMap(nums,target);
+        int[] result = twoSum(nums,target);
 
         for(int i=0; i < result.length; i++){
             System.out.println(result[i]);
