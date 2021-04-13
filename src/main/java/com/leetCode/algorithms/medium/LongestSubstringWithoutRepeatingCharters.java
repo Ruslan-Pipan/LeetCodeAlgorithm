0e54df07b7e87402b40e3f07e478f518a77a3807
ruspi <rus.pipan@ukr.net>
2021-04-13 13:47:52 +0300

    Algorithm task: Longest substring Without repeating charter.

--- src/main/java/com/leetCode/algorithms/medium/LongestSubstringWithoutRepeatingCharters.java
+++ src/main/java/com/leetCode/algorithms/medium/LongestSubstringWithoutRepeatingCharters.java
@@ -1,4 +1,33 @@
 package com.leetCode.algorithms.medium;
 
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharters {
+
+    public static int lengthLongerSubstring(String s){
+        HashSet<Character> characters = new HashSet<>();
+
+        int max = 0;
+        int count = 0;
+        int currentPosition = 0;
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (!characters.contains(chars[i])){
+                characters.add(chars[i]);
+                ++count;
+            }else {
+                characters.clear();
+                if (count > max) max = count;
+                count = 0;
+                i = currentPosition++;
+            }
+        }
+
+        return Math.max(max, count);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthLongerSubstring(" "));
+    }
 }
